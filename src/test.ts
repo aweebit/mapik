6552f4dcf911525f92d4cfac45f7e193e97d58a4
Wee Bit <aweebit64@gmail.com>
2026-08-05 04:45:26 +0200

    test

--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,151 @@
-import { Schema } from "effect";
+import type { Table } from "drizzle-orm";
+import {
+  bigint,
+  doublePrecision,
+  primaryKey,
+  snakeCase,
+  text,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { Effect, Schema, SchemaParser, SchemaTransformation } from "effect";
+import {
+  createDeepenAtDelimiter,
+  type DeepenAtDelimiter,
+} from "./deepenAtDelimiter.js";
+import {
+  createDrizzleMapper,
+  DrizzleTableMapper,
+  type MapToSelf,
+} from "./drizzle.js";
+import { drizzle } from "drizzle-orm/node-postgres";
 
-const schema = Schema.BigInt;
+const Optional = <S extends Schema.Top>(schema: S) => {
+  const from = Schema.Union([
+    Schema.Struct({ present: Schema.Literal(false) }),
+    Schema.Struct({
+      present: Schema.Literal(true),
+      // Union strips mutability & optionality
+      value: Schema.Union([Schema.toEncoded(schema)]),
+    }),
+  ]);
+  const to = Schema.optional(Schema.toType(schema));
+  return from.pipe(
+    Schema.decodeTo(
+      to,
+      SchemaTransformation.transformOrFail({
+        decode: (obj, options) =>
+          obj.present
+            ? SchemaParser.decodeEffect(schema)(obj.value, {
+                ...options,
+                disableChecks: true,
+              })
+            : Effect.succeed(undefined),
+        encode: (value, options) =>
+          value === undefined
+            ? Effect.succeed({ present: false })
+            : SchemaParser.encodeEffect(schema)(value, {
+                ...options,
+                disableChecks: true,
+              }).pipe(Effect.map((value) => ({ present: true, value }))),
+      }),
+    ),
+  );
+};
 
-const result = Schema.decodeUnknownSync(schema)(null, { disableChecks: true });
-// console.log(result);
+const Vector3d = Schema.Tuple([Schema.Number, Schema.Number, Schema.Number]);
+
+class Experiment extends Schema.Class<Experiment>("Experiment")({
+  name: Schema.String,
+  timeStarted: Schema.Date,
+  timeEnded: Optional(Schema.Date),
+}) {}
+
+class ExperimentData extends Schema.Class<ExperimentData>("ExperimentData")({
+  experimentId: Schema.BigInt,
+  timestamp: Schema.Date,
+  pressure: Schema.Number,
+  acceleration: Schema.Struct({
+    top: Vector3d,
+    bottom: Vector3d,
+  }),
+}) {}
+
+const experiment = snakeCase.table("experiment", {
+  id: bigint({ mode: "bigint" }).primaryKey().generatedAlwaysAsIdentity(),
+  name: text().notNull(),
+  timeStarted: timestamp({ withTimezone: true }).notNull(),
+  timeEnded: timestamp({ withTimezone: true }),
+});
+
+const experimentData = snakeCase.table(
+  "experiment_data",
+  {
+    experimentId: bigint({ mode: "bigint" })
+      .notNull()
+      .references(() => experiment.id),
+    timestamp: timestamp({ withTimezone: true }).notNull(),
+    pressure: doublePrecision().notNull(),
+    acceleration_top_x: doublePrecision().notNull(),
+    acceleration_top_y: doublePrecision().notNull(),
+    acceleration_top_z: doublePrecision().notNull(),
+    acceleration_bottom_x: doublePrecision().notNull(),
+    acceleration_bottom_y: doublePrecision().notNull(),
+    acceleration_bottom_z: doublePrecision().notNull(),
+  },
+  (table) => [primaryKey({ columns: [table.experimentId, table.timestamp] })],
+);
+
+const underscoreDeepen = createDeepenAtDelimiter("_");
+
+const underscoreMake = <T extends Table>(
+  table: T,
+): DrizzleTableMapper<T, DeepenAtDelimiter<"_", MapToSelf<T>>> =>
+  DrizzleTableMapper.make(table, underscoreDeepen);
+
+const createUnderscoreDrizzleMapper = <const Ts extends readonly Table[]>(
+  tables: Ts,
+) => {
+  return createDrizzleMapper(
+    tables.map(underscoreMake) as {
+      [K in keyof Ts]: DrizzleTableMapper<
+        Ts[K],
+        DeepenAtDelimiter<"_", MapToSelf<Ts[K]>>
+      >;
+    },
+  );
+};
+
+const mapper = createUnderscoreDrizzleMapper([experiment, experimentData]);
+
+const newExperiment = new Experiment({ name: "#1", timeStarted: new Date() });
+
+const db = drizzle("TBD");
+
+const experimentId = (
+  await db
+    .insert(experiment)
+    .values(
+      mapper(experiment).flatten({
+        ...newExperiment,
+        timeEnded: newExperiment.timeEnded ?? null,
+      }),
+    )
+    .returning({ id: experiment.id })
+)[0]!.id;
+
+const newExperimentData = new ExperimentData({
+  experimentId,
+  timestamp: new Date(),
+  pressure: 0,
+  acceleration: { top: [0, 0, 0], bottom: [0, 0, 0] },
+});
+
+await db.insert(experimentData).values(
+  mapper(experimentData).flatten({
+    ...newExperimentData,
+    acceleration: (({ top, bottom }) => ({
+      top: (([x, y, z]) => ({ x, y, z }))(top),
+      bottom: (([x, y, z]) => ({ x, y, z }))(bottom),
+    }))(newExperimentData.acceleration),
+  }),
+);
