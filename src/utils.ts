eb468a2b086a0cb32587eace4cd8ff8bd378a6fa
Wee Bit <aweebit64@gmail.com>
2026-08-05 04:45:19 +0200

    Settle for simpler DeclaredValueOf implementation after tracing
    There was no noticeable difference in performance between the two implementations

--- src/utils.ts
+++ src/utils.ts
@@ -9,13 +9,6 @@ export type DeclaredValueOf<T, K extends keyof T = keyof T> = Required<
   Pick<T, K>
 >[K];
 
-// // Maybe this is better in terms of performance, maybe not. TODO: Investigate
-// export type DeclaredValueOf<T, K extends keyof T = keyof T> = T extends {
-//   readonly [P in K]?: infer Result;
-// }
-//   ? Result
-//   : never;
-
 export type DeepRecord<K extends PropertyKey, T> = {
   [P in K]: T | DeepRecord<K, T>;
 };
